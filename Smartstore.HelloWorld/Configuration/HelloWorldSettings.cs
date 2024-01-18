f967a9127219b94e83510f0bdb288ae45504e017
Dimitrios Porfiropoulos <dimiporf@live.com>
2024-01-18 18:47:48 +0100

    This is for local training - IGNORE

---
+++ Smartstore.HelloWorld/Configuration/HelloWorldSettings.cs
@@ -0,0 +1,9 @@
+﻿using Smartstore.Core.Configuration;
+
+namespace Smartstore.HelloWorld.Settings
+{
+    public class HelloWorldSettings : ISettings
+    {
+        public string Name { get; set; } = "Dimitrios Porfyropoulos";
+    }
+}
